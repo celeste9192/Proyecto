4185076c836e54970ce644bf09d7b799bd2d2a41
gabitoher2506 <144975521+gabitoher2506@users.noreply.github.com>
2024-03-13 18:34:26 -0600

    Update clientes.php

--- php/clientes.php
+++ php/clientes.php
@@ -1,103 +1,50 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <title>Clientes</title>
     <style>
-        body,
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6,
-        p,
-        ul,
-        li,
-        button,
-        input,
-        form,
-        label {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
         body {
             font-family: 'Poppins', sans-serif;
             color: #31241E;
             background-color: #F6F4F3;
+            margin: 0;
+            padding: 0;
         }
 
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-            font-family: 'Montserrat', sans-serif;
-            font-weight: bold;
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+            padding: 20px;
         }
 
         header {
             background-color: #F6F4F3;
             padding: 20px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
             border-bottom: 1px solid #31241E;
         }
 
         h1 {
             font-size: 36px;
+            text-align: center;
+            margin-bottom: 20px;
             text-transform: uppercase;
         }
 
-        nav ul {
-            list-style-type: none;
-            padding: 0;
+        .btn-container {
+            display: flex;
+            justify-content: center;
+            margin-bottom: 20px;
         }
 
-        nav ul li {
-            display: inline-block;
-            margin-right: 20px;
-        }
-
-        nav ul li a {
+        .btn-container a {
+            margin: 0 10px;
             text-decoration: none;
-            font-family: 'Montserrat', sans-serif;
-            font-weight: bold;
-            font-size: 16px;
-            color: #31241E;
         }
 
-        form {
-            margin-top: 20px;
-        }
-
-        label {
-            display: block;
-            font-weight: bold;
-            margin-bottom: 5px;
-        }
-
-        input[type="text"],
-        input[type="number"],
-        input[type="email"],
-        input[type="url"],
-        textarea {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 10px;
-            border-radius: 5px;
-            border: 1px solid #D1C8C1;
-        }
-
-        input[type="submit"],
-        button {
+        .btn {
             padding: 10px 20px;
             border: none;
             border-radius: 5px;
@@ -107,76 +54,91 @@
             font-weight: bold;
             font-size: 18px;
             cursor: pointer;
+            transition: background-color 0.3s ease;
         }
 
-        button {
-            background-color: transparent;
+        .btn:hover {
+            background-color: #31241E;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+        }
+
+        th, td {
+            border: 1px solid #31241E;
+            padding: 10px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #31241E;
+            color: #FFF;
+            text-transform: uppercase;
+        }
+
+        td a {
             color: #31241E;
+            font-weight: bold;
+            text-decoration: none;
         }
 
-        h1 {
-            text-align: center;
-            margin-top: 20px;
-            margin-bottom: 20px;
-        }
-
-        ul {
-            margin-top: 20px;
-            padding-left: 20px;
-        }
-
-        ul li {
-            margin-bottom: 20px;
+        td a:hover {
+            text-decoration: underline;
         }
     </style>
 </head>
-
 <body>
-    <h1>Clientes</h1>
+    <header>
+        <h1>Clientes</h1>
+    </header>
+    <div class="container">
+        <div class="btn-container">
+            <a href="agregar_cliente.php" class="btn">Agregar Cliente</a>
+            <a href="eliminar_cliente.php" class="btn">Eliminar Cliente</a>
+            <a href="editar_cliente.php" class="btn">Editar Cliente</a>
+        </div>
 
-    <a href="agregar_cliente.php"><button>Agregar Cliente</button></a>
-    <a href="eliminar_cliente.php"><button>Eliminar Cliente</button></a>
-    <a href="editar_cliente.php"><button>Editar Cliente</button></a>
+        <?php
+            include 'conexion.php';
 
-    <?php
-    include 'conexion.php';
+            function leerClientes()
+            {
+                $conexion = Conecta();
+                $sql = "SELECT * FROM Clientes";
+                $resultado = mysqli_query($conexion, $sql);
 
-    function leerClientes()
-    {
-        $conexion = Conecta();
-        $sql = "SELECT * FROM Clientes";
-        $resultado = mysqli_query($conexion, $sql);
+                if ($resultado && mysqli_num_rows($resultado) > 0) {
+                    echo "<h2>Listado de Clientes</h2>";
+                    echo "<table>";
+                    echo "<tr><th>ID</th><th>Nombre</th><th>Apellido</th><th>Dirección</th><th>Ciudad</th><th>País</th><th>Correo Electrónico</th><th>Teléfono</th></tr>";
 
-        if ($resultado && mysqli_num_rows($resultado) > 0) {
-            echo "<h2>Listado de Clientes</h2>";
-            echo "<table border='1'>";
-            echo "<tr><th>ID</th><th>Nombre</th><th>Apellido</th><th>Dirección</th><th>Ciudad</th><th>País</th><th>Correo Electrónico</th><th>Teléfono</th></tr>";
+                    while ($fila = mysqli_fetch_assoc($resultado)) {
+                        echo "<tr>";
+                        echo "<td>" . $fila['cliente_id'] . "</td>";
+                        echo "<td>" . $fila['nombre'] . "</td>";
+                        echo "<td>" . $fila['apellido'] . "</td>";
+                        echo "<td>" . $fila['direccion'] . "</td>";
+                        echo "<td>" . $fila['ciudad'] . "</td>";
+                        echo "<td>" . $fila['pais'] . "</td>";
+                        echo "<td><a href='mailto:" . $fila['correo_electronico'] . "'>" . $fila['correo_electronico'] . "</a></td>";
+                        echo "<td>" . $fila['telefono'] . "</td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                } else {
+                    echo "No se encontraron clientes.";
+                }
 
-            while ($fila = mysqli_fetch_assoc($resultado)) {
-                echo "<tr>";
-                echo "<td>" . $fila['cliente_id'] . "</td>";
-                echo "<td>" . $fila['nombre'] . "</td>";
-                echo "<td>" . $fila['apellido'] . "</td>";
-                echo "<td>" . $fila['direccion'] . "</td>";
-                echo "<td>" . $fila['ciudad'] . "</td>";
-                echo "<td>" . $fila['pais'] . "</td>";
-                echo "<td>" . $fila['correo_electronico'] . "</td>";
-                echo "<td>" . $fila['telefono'] . "</td>";
-                echo "</tr>";
+                Desconectar($conexion);
             }
-            echo "</table>";
-        } else {
-            echo "No se encontraron clientes.";
-        }
 
-        Desconectar($conexion);
-    }
+            leerClientes();
+        ?>
 
-    leerClientes();
-    
-    ?>
-    <a href="index.php"><button>Menu Principal</button></a>
-
+        <a href="index.php" class="btn">Menu Principal</a>
+    </div>
 </body>
-
 </html>
